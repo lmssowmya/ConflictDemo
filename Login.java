6a2a696a7db3f673a129446f95d85bf9f171b8fe
Locharla Moda Satya Sowmya <locharla.moda.satya.sowmya@ibm.com>
2023-04-14 11:53:40 +0530

    IN GITHUB

---
+++ Login.java
@@ -0,0 +1,16 @@
+public class Login{
+
+	public void verifyUser{
+		String username,password;
+		//connect to database
+			if(rs.get String(1).equals(rs.get String(2))){
+				Sysyem.out.println("sowmya");
+			}
+			else{
+				Sysyem.out.println("Please try again");
+			}
+			
+	}
+
+
+}
